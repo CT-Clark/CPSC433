7ab241503378480431db2c2ba3b79890765edfc3
Cody Clark <cody.t.clark@gmail.com>
2018-11-05 20:52:03 -0700

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,14 +1,24 @@
+/**
+ * Authors: Cody Clark, 
+ * Date last modified: Nov 5, 2018
+ */
+
 public class Project {
   
   // Function that tests whether an assignment of courses is valid
-  public bool Constr() {
+  // Takes in an Assign class instance
+  public bool Constr(Assign assignment) {
+    bool result;
     
-    return foo; 
+    return result; 
   }
   
   // Measures the soft constraints of an assignment
-  public int Eval() {
-    return foo; 
+  // Takes in an assign class instance
+  public int Eval(Assign assignment) {
+    int result;
+    
+    return result; 
   }
   
   public static void main(String[] args) {
