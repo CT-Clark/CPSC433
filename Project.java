b9d2897510fcdef4e72bcec5d94da78ccc856ea8
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 14:36:26 -0700

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,6 +1,8 @@
 /**
- * Authors: Cody Clark, 
+ * Authors: Cody Clark, Lily Pollreis, Marcel Baier, Stephen Booth, Christna Eder
  * Date last modified: Nov 6, 2018
+ *
+ * (Fill in project description here)
  */
 
 public class Project {
@@ -22,13 +24,18 @@ public class Project {
   }
   
   public static void main(String[] args) {
-  // Instantiate classes
+  // Instantiate classes i.e. create instances of list objects with null values
     
-  // Parse input
+  // Parse input - create instances of course and lab objects that correspond to the items parsed in
+  // We need to decide how we're storing the hard and soft constraints
+  // We could pass them to Eval and Constr as a list?
   
-  // Run search
+  // Run search - Use Atree branch and bound search algorithm
+  // Search all of the possible assignments for the one with the lowest Eval value, pursue that branch
+  // Until we find a complete assign that has an Eval-value lower than every other option
+  // We only create branches that do not violate Constr
     
-  // Output result
+  // Output result - See example output text file for formatting
 
   }
 }
