d336ca9f38807797ce74b3af2e594d2acd51f342
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 14:23:35 -0700

    Update Assign.java

--- Assign.java
+++ Assign.java
@@ -1,3 +1,7 @@
+// This is the class that contains partial to full assignments of the schedule
+
 public class Assign {
+  // TODO: Decide what data structures we're using for this.
+  // Are we using a list of Slots with the slots having list attributes of the courses/labs assigned?
 
 }
