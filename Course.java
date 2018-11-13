d6f40b3405bf95fd1596bd62acc1c0b46a6c2fa5
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 15:30:38 -0700

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,7 +1,5 @@
 public class Course {
   private String department; // CPSC or SENG
   private String number; // e.g. 433
-  private String type; // LEC/LAB
   private String lectureNumber; // e.g. 01 or 02
-  private String labNumber; // Is not null only if type == LAB
 }
