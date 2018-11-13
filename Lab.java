6471f0c19bedd34178cd22d65e8e9b141e9f00f5
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 15:32:23 -0700

    Update Lab.java

--- Lab.java
+++ Lab.java
@@ -1,4 +1,6 @@
 public class Lab {
-
-
+  private String department; // CPSC/SENG
+  private String number; // e.g. 433
+  private String lectureNumber; // So that we know which lecture slot this corresponds to
+  private string labNumber; // e.g. 01, 03
 }
