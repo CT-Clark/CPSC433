3125a6a5d2544efa6616acaf924b15e7501e5e73
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 15:54:16 -0700

    Update Lab.java

--- Lab.java
+++ Lab.java
@@ -2,5 +2,37 @@ public class Lab {
   private String department; // CPSC/SENG
   private String number; // e.g. 433
   private String lectureNumber; // So that we know which lecture slot this corresponds to
-  private string labNumber; // e.g. 01, 03
+  private String labNumber; // e.g. 01, 03
+  
+  public String getDepartment() {
+    return department;
+  }
+  
+  public String getNumber() {
+    return number;
+  }
+  
+  public String getLectureNumber() {
+    return lectureNumber;
+  }
+
+  public String getLabNumber() {
+    return labNumber;
+  }
+  
+  public void setDepartment(String s) {
+    this.department = s;
+  }
+  
+  public void setNumber(String s) {
+    this.number = s;
+  }
+  
+  public void setLectureNumber(String s) {
+    this.lectureNumber = s;
+  }
+  
+  public void setLabnumber(String s) {
+    this.labNumber = s;
+  }
 }
