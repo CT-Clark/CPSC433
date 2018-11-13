6caa8e528a86c170c34376ca4bf1e048a9c350ce
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 16:48:18 -0700

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,29 +1,3 @@
-public class Course {
-  private String department; // CPSC or SENG
-  private String number; // e.g. 433
-  private String lectureNumber; // e.g. 01 or 02
+public class Course extends CourseLab {
   
-  public String getDepartment() {
-    return department;
-  }
-  
-  public String getNumber() {
-    return number;
-  }
-  
-  public String getLectureNumber() {
-    return lectureNumber;
-  }
-  
-  public void setDepartment(String s) {
-    this.department = s;
-  }
-  
-  public void setNumber(String s) {
-    this.number = s;
-  }
-  
-  public void setLectureNumber(String s) {
-    this.lectureNumber = s;
-  }
 }
