9290bf18fc4edc93456b65cf683bb37c82207ee5
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 17:04:13 -0700

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,6 +1,7 @@
 // Object which represent the state of the assignment - from a partial assignment to the final answer
 // Contains all of the Slot objects and their partical assignments
+import java.util.List;
 
 public class Assignment {
-  
+  List<Object> slots = new List<Object>;
 }
