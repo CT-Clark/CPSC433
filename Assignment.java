dd8d8fb08444733beb925d025dc30f167ef821cc
Cody Clark <cody.t.clark@gmail.com>
2018-11-13 17:03:34 -0700

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,1 +1,6 @@
-public class Assignment
+// Object which represent the state of the assignment - from a partial assignment to the final answer
+// Contains all of the Slot objects and their partical assignments
+
+public class Assignment {
+  
+}
