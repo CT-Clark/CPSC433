630dacb0fce96d88f70a3658d2025574d74eb2c8
Cody Clark <cody.t.clark@gmail.com>
2018-11-25 22:01:48 -0700

    Update Project.java

--- src/Project.java
+++ src/Project.java
@@ -7,7 +7,7 @@
 
 public class Project {
   // Search control
-  public Assignment kontrol(Assignment a) {
+  public Assignment kontrol(Assignment assign) {
     return a;
   }
   
@@ -15,17 +15,32 @@ public class Project {
   
   // Function that tests whether an assignment of courses is valid
   // Takes in an Assignment class instance
-  public boolean Constr(Assignment a) {
+  public boolean Constr(Assignment assign) {
     boolean result;
+    // Test that not more than courseMax courses are assigned to a slot
+    // Test that not more than labMax labs are assigned to a slot
+    // Labs cannot be scheduled at the same time as courses
+    // Test that non-compatible courses are not assigned together
+    // The final assignment correctly uses the partial assignment parsed into it
+    // Test that unwanted courses are not assigned to their unwanted slots
+    // Test that courses assigned to a Monday slot are also assigned to a We and Fr slot
+    // If a course is assigned to a slot on Tuesday, it also has to be assigned to the respective Thu slot
+    // Test that all LEC 9 section number courses are slotted into evening slots
+    // Test that all 500 level courses are assigned to different slots
+    // No courses can be scheduled on Tuesday 11:00-12:30
+    // Test that CPSC 813 and CPSC 913 are scheduled Tu/Th 18:00-19:00 and CPSC 313 and CPSC 413 coursesélabs are not
+    // scheduled during or overlapping these times
     
-    return false;
+    return result;
   }
   
   // Measures the soft constraints of an assignment
   // Takes in an assignment class instance
-  public int Eval(Assignment a) {
+  public int Eval(Assignment assign) {
     int result = 0;
     
+    result += pen_labmin;
+    result += pen_coursemin;
     return result; 
   }
   
