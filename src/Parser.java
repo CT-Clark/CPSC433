0d907e876a38461d290fb2cad384ffc5072b90bb
Cody Clark <cody.t.clark@gmail.com>
2018-11-25 22:29:00 -0700

    Update Parser.java
    Added select comments

--- src/Parser.java
+++ src/Parser.java
@@ -7,16 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// Parses the input file
+
 public class Parser {
     private String path;
-    private List<Lecture> coursesList;
-    private List<Lecture> labsList;
-    private List<Slot> courseSlots;
-    private List<Slot> labSlots;
-    private List<Pair> pairs;
-    private List<Pair> not_compatible;
+    private List<Lecture> coursesList; // List of coures
+    private List<Lecture> labsList;    // List of labs
+    private List<Slot> courseSlots;    // List of slots courses can fit into
+    private List<Slot> labSlots;       // List of slots labs can fit into
+    private List<Pair> pairs;          // List of courses/labs that should be paired at the same time
+    private List<Pair> not_compatible; // List of courses/labs that cannot be scheduled at the same time
 
     public Parser(String path) {
+        // Set info for parser
         this.path = path;
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
@@ -26,6 +29,7 @@ public class Parser {
         }
     }
 
+    // Parse input and set appropriate fieds
     public SearchControl parseInput() throws IOException {
         try (Scanner scanner = new Scanner(new File(path))) {
             List<String> inputFile = Files.readAllLines(Paths.get(path));
@@ -35,41 +39,52 @@ public class Parser {
             for (String line : inputFile) {
                 line = line.toLowerCase();
                 switch (line) {
+                    // Name of the department
                     case "name:":
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate list of course slots
                     case "course slots:":
                         saveName(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate list of lab slots
                     case "lab slots:":
                         saveCourseSlots(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate list of courses
                     case "courses:":
                         saveLabSlots(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate list of labs
                     case "labs:":
                         saveCourses(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate list of not-compatible courses/labs
                     case "not compatible:":
                         saveLabs(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate list of courses that cannot be assigned to a particular slot
                     case "unwanted:":
                         saveNotCompatible(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate the list of courses/tutorials that should be put into a specific slot
+                    // Along with the Eval penalty if they're not
                     case "preferences:":
                         saveUnwanted(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate the list of courses/labs that should be placed int othe same slot
                     case "pair:":
                         savePreferences(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
                         break;
+                    // Generate a partial assignment to start with
                     case "partial assignments:":
                         savePairs(inputFile, lastBreakpoint, counter);
                         lastBreakpoint = counter + 1;
@@ -80,11 +95,13 @@ public class Parser {
             savePartialAssignments(inputFile, lastBreakpoint, counter - 1);
         }
         List<Lecture> lectures = new ArrayList<>();
-        lectures.addAll(coursesList);
-        lectures.addAll(labsList);
+        lectures.addAll(coursesList); // Add courses
+        lectures.addAll(labsList);    // Add labs
+        // Return the data structure that contains all of the info for the parsed items
         return new SearchControl(lectures, courseSlots, labSlots, not_compatible, pairs);
     }
 
+    // Save an initial state to start from
     private void savePartialAssignments(List<String> inputFile, int lastBreakpoint, int i) {
         //TODO: save partial assignment as start state
         for (String line : inputFile.subList(lastBreakpoint, i)) {
@@ -99,11 +116,13 @@ public class Parser {
 
         }
     }
-
+    
+    // Save the items that should be paired
     private void savePairs(List<String> inputFile, int lastBreakpoint, int counter) {
         pairs = savePairedLectures(inputFile, lastBreakpoint, counter);
     }
 
+    // Save the courses/labs that should be placed into specific slots
     private void savePreferences(List<String> inputFile, int lastBreakpoint, int counter) {
         for (String line : inputFile.subList(lastBreakpoint, counter)) {
             if(line.isEmpty()) {
@@ -111,31 +130,36 @@ public class Parser {
             }
             String[] preference = line.split(",");
 
+            // Create the course/lab item
             Lecture lec1 = Lecture.produceLecture(preference[2]);
+            // Create the slot with a course/lab type
             Slot slot = GeneralSlot.produceSlot(preference[0] + ", " + preference[1], lec1 instanceof Lab ? !GeneralSlot.COURSE : GeneralSlot.COURSE);
 
             if (lec1 instanceof Lab) {
                 Lecture lab = labsList.get(labsList.indexOf(lec1));
                 lab.preferedSlots.add(labSlots.get(labSlots.indexOf(slot)));
-                lab.preferenceScore = Integer.valueOf(preference[3].replace(" ", ""));
+                lab.preferenceScore = Integer.valueOf(preference[3].replace(" ", "")); // Eval value to use
 
             } else {
                 Lecture lab = coursesList.get(coursesList.indexOf(lec1));
                 lab.preferedSlots.add(courseSlots.get(courseSlots.indexOf(slot)));
-                lab.preferenceScore = Integer.valueOf(preference[3].replace(" ", ""));
+                lab.preferenceScore = Integer.valueOf(preference[3].replace(" ", "")); // Eval value to use
             }
         }
     }
 
+    // Save the courses/labs that cannot be assigned to a particular slot
     private void saveUnwanted(List<String> inputFile, int lastBreakpoint, int counter) {
         for (String line : inputFile.subList(lastBreakpoint, counter)) {
             if(line.isEmpty()) {
                 continue;
             }
             String[] unwanted = line.split(",", 2);
-            Lecture lec1 = Lecture.produceLecture(unwanted[0]);
+            Lecture lec1 = Lecture.produceLecture(unwanted[0]); // Create course/lab object
 
+            // Create the slot
             Slot slot = GeneralSlot.produceSlot(unwanted[1], lec1 instanceof Lab ? !GeneralSlot.COURSE : GeneralSlot.COURSE);
+            // ** Is this if-else necessary? ** //
             if(slot.getType() == GeneralSlot.COURSE) {
                 courseSlots.get(courseSlots.indexOf(slot)).saveUnwantedLecture(lec1);
             } else {
@@ -144,10 +168,12 @@ public class Parser {
         }
     }
 
+    // Save the courses/labs which are not compatible
     private void saveNotCompatible(List<String> inputFile, int lastBreakpoint, int counter) {
         not_compatible = savePairedLectures(inputFile, lastBreakpoint, counter);
     }
 
+    // Save the courses/labs that should occur in the same slot
     private List<Pair> savePairedLectures(List<String> inputFile, int lastBreakpoint, int counter) {
         List<Pair> couples = new ArrayList<>(counter - lastBreakpoint + 1);
         for (String line : inputFile.subList(lastBreakpoint, counter)) {
@@ -175,6 +201,7 @@ public class Parser {
         return couples;
     }
 
+    // Save the labs
     private void saveLabs(List<String> inputFile, int lastBreakpoint, int counter) {
         labsList = new ArrayList<>(inputFile.size());
         for (String line : inputFile.subList(lastBreakpoint, counter)) {
@@ -186,6 +213,7 @@ public class Parser {
         }
     }
 
+    // Save the courses
     private void saveCourses(List<String> inputFile, int lastBreakpoint, int counter) {
         coursesList = new ArrayList<>(inputFile.size());
         for (String line : inputFile.subList(lastBreakpoint, counter)) {
@@ -196,14 +224,17 @@ public class Parser {
         }
     }
 
+    // Save the lab slots
     private void saveLabSlots(List<String> inputFile, int lastBreakpoint, int counter) {
         labSlots = saveSlots(inputFile, lastBreakpoint, counter, !GeneralSlot.COURSE);
     }
 
+    // Save the course slots
     private void saveCourseSlots(List<String> inputFile, int lastBreakpoint, int counter) {
         courseSlots = saveSlots(inputFile, lastBreakpoint, counter, GeneralSlot.COURSE);
     }
 
+    // Method for saving the course/lab slots
     private List<Slot> saveSlots(List<String> inputFile, int lastBreakpoint, int counter, boolean type) {
         List<Slot> slotList = new ArrayList<>(inputFile.size());
         for (String line : inputFile.subList(lastBreakpoint, counter)) {
@@ -216,6 +247,7 @@ public class Parser {
         return slotList;
     }
 
+    // Save the name of the assignment
     private void saveName(List<String> inputFile, int lastBreakpoint, int counter) {
         for (String line : inputFile.subList(lastBreakpoint, counter)) {
             System.out.println(line);
